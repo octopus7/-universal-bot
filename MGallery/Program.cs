33229616bacf0d638a1657ec79c869441859f6a8
octopus7 <octopus7net@gmail.com>
2022-08-27 01:51:05 +0900

    r100 mode

--- MGallery/Program.cs
+++ MGallery/Program.cs
@@ -1,8 +1,11 @@
 ﻿// */1 * * * * /home/pi/.dotnet/dotnet /home/pi/mgall/genshin/MGallery.dll
 
+using System.Reflection;
+
 string gallid = "onshinproject";
+
 string urlMain = $"https://gall.dcinside.com/mgallery/board/lists/?id={gallid}";
-string urlRecommend = $"https://gall.dcinside.com/mgallery/board/lists?id={gallid}&exception_mode=recommend";
+string urlRecommend = $"https://gall.dcinside.com/mgallery/board/lists/?id={gallid}&exception_mode=recommend";
 
 HttpClient client = new HttpClient();
 
@@ -19,6 +22,13 @@ if (assembly != null)
     Console.WriteLine($"main : {urlMain}");
     Console.WriteLine($"recommend : {urlRecommend}");
 
+    await R100(urlMain, urlRecommend, client, assembly, time, date);
+    //await M4R1(urlMain, urlRecommend, client, assembly, time, date);
+
+}
+
+static async Task M4R1(string urlMain, string urlRecommend, HttpClient client, Assembly? assembly, string time, string date)
+{
     string? exePath = assembly.Location;
     exePath = Path.Combine(Path.GetDirectoryName(exePath), date);
     Console.WriteLine("exePath : " + exePath);
@@ -26,13 +36,30 @@ if (assembly != null)
     if (!Directory.Exists(exePath)) Directory.CreateDirectory(exePath);
     Directory.SetCurrentDirectory(exePath);
 
-    //string? 
-    string? htmlRecommend = await client.GetStringAsync(urlRecommend);
-
-    for(int page = 1; page<=4; page++)
+    for (int page = 1; page <= 4; page++)
     {
-        File.WriteAllText($"m_{time}_{page}.htm", await client.GetStringAsync($"{urlMain}&page={page}"));
-    }    
-    File.WriteAllText("r_" + time  + ".htm", htmlRecommend);
+        var url = $"{urlMain}&page={page}";
+        Console.WriteLine(url);
+        File.WriteAllText($"m_{time}_{page}.htm", await client.GetStringAsync(url));
+    }
+
+    Console.WriteLine(urlRecommend);
+    File.WriteAllText($"r_{time}.htm", await client.GetStringAsync(urlRecommend));
 }
 
+
+static async Task R100(string urlMain, string urlRecommend, HttpClient client, Assembly? assembly, string time, string date)
+{
+    string? exePath = assembly.Location;
+    exePath = Path.Combine(Path.GetDirectoryName(exePath), date + "r100");
+    Console.WriteLine("exePath : " + exePath);
+    Console.WriteLine();
+    if (!Directory.Exists(exePath)) Directory.CreateDirectory(exePath);
+    Directory.SetCurrentDirectory(exePath);
+    for (int page = 1; page <= 100; page++)
+    {
+        var url = $"{urlRecommend}&page={page}";
+        Console.WriteLine(url);
+        File.WriteAllText($"r_{time}_{page}.htm", await client.GetStringAsync(url));
+    }
+}
